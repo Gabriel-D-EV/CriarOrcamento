44b12f88ed533a5283c9298f3bc080dc56647ae8
Gabriel Silva <gabriel.silva_dev@outlook.com>
2023-11-20 17:14:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,33 @@
 import customtkinter as ctk
 from tkinter import *
+from fpdf import FPDF
 
 
 def click_():
-    ctk.Label.config('click')
+    nome_= nome.get()
+    projeto_ = projeto.get()
+    hpre_ = hpre.get()
+    valorh_ = valorh.get()
+    prazo_ = prazo.get()
+
+    total = float(hpre_) * float(valorh_)
+    
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Arial', 'B', 16)
+    pdf.image('./img/formulario.png', x=0, y=0)
+
+    pdf.text(115, 145, projeto_)
+    pdf.text(115, 160, f'{hpre_}h')
+    pdf.text(115, 175, f'{valorh_:.2f}')
+    pdf.text(115, 190, prazo_)
+    pdf.text(120, 205, f'{total:.2f}')
+    pdf.text(120, 225, f'Assinatura do(a) {nome_}')
+
+    pdf.output(f'Orçamento do {nome_}.pdf')
+    
+
+    
 
 ctk.set_appearance_mode('System')
 ctk.set_default_color_theme('dark-blue')
@@ -18,24 +42,43 @@ img = PhotoImage(file='./img/gbh-tech.png')
 label_img = ctk.CTkLabel(master=app, image=img)
 label_img.place(x=1, y=0)
 
-
+#Front-end
 
 frame = ctk.CTkFrame(master=app, width=300, height=300)
-frame.pack()
 frame.place(x=300, y=0)
 
-projeto = ctk.CTkEntry(master=frame, placeholder_text="Projeto:",width=200, font=('Reboco', 14)).place(x=50, y=40)
+nome = ctk.CTkEntry(master=frame, placeholder_text="Nome:",width=200, font=('Reboco', 14)).place(x=50, y=20)
+
+projeto = ctk.CTkEntry(master=frame, placeholder_text="Projeto:",width=200, font=('Reboco', 14)).place(x=50, y=50)
 
 hpre = ctk.CTkEntry(master=frame, placeholder_text="Horas Previstas:",width=200, font=('Reboco', 14)).place(x=50, y=80)
 
-valorh = ctk.CTkEntry(master=frame, placeholder_text="Valor da Hora:",width=200, font=('Reboco', 14)).place(x=50, y=120)
+valorh = ctk.CTkEntry(master=frame, placeholder_text="Valor da Hora:",width=200, font=('Reboco', 14)).place(x=50, y=110)
 
-prazo = ctk.CTkEntry(master=frame, placeholder_text="Prazo de Entrega:",width=200, font=('Reboco', 14)).place(x=50, y=160)
-#totvalor = 
+prazo = ctk.CTkEntry(master=frame, placeholder_text="Prazo de Entrega:",width=200, font=('Reboco', 14)).place(x=50, y=140)
 
 
-btn = Button(master=frame, text='Gerar Orçamento', command=click_)
-btn.place(x=100,y=240)
+tot = ctk.CTkLabel(master=frame, text='Valor total: ' + str(hpre * valorh),font=('Reboco', 18)).place(x=50, y=190) 
+
+
+btn = ctk.CTkButton(master=frame, text='Gerar Orçamento', command=click_)
+btn.place(x=80,y=240)
+
+#Back-end
+
+    
+
+
+
+
+
+
+#importar pdf
+
+
+
+
+
 
 
 app.mainloop()
